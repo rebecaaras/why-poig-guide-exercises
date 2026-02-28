3134983cfb3fe160828e35c36f3ee93b83f392a0
rebecaaras <rebecaaraujo2013@gmail.com>
2026-02-28 16:47:04 -0300

    code to encode and save evil ideas

---
+++ chapter4/evil_ideas_app.rb
@@ -0,0 +1,16 @@
+require_relative "wordlist"
+
+print "Enter your new idea: "
+idea = gets
+
+# Swap evil idea in code words
+CODE_WORDS.each do |real, code|
+  idea.gsub!( real, code)
+end
+
+# Save the jibberish to a new file
+print "File encoded. Please enter a name for this idea: "
+idea_name = gets.strip
+File::open( "./ideas/idea-" + idea_name + ".txt", "w") do |f|
+  f << idea
+end
