e82b629e206aeefe8e1b507e34f249eb22e267cb
rebecaaras <rebecaaraujo2013@gmail.com>
2026-03-01 19:15:42 -0300

    arrange decode and encode into functions

--- chapter4/evil_ideas_app.rb
+++ chapter4/evil_ideas_app.rb
@@ -1,16 +1,36 @@
 require_relative "wordlist"
 
-print "Enter your new idea: "
-idea = gets
-
-# Swap evil idea in code words
-CODE_WORDS.each do |real, code|
-  idea.gsub!( real, code)
+def encodes_idea
+  print "Enter your new idea: "
+  idea = gets
+  
+  # Swap evil idea in code words
+  CODE_WORDS.each do |real, code|
+    idea.gsub!( real, code)
+  end
+  
+  # Save the jibberish to a new file
+  print "File encoded. Please enter a name for this idea: "
+  idea_name = gets.strip
+  File::open( "./ideas/idea-" + idea_name + ".txt", "w") do |f|
+    f << idea.upcase.reverse.gsub!(" ", "")
+  end
 end
 
-# Save the jibberish to a new file
-print "File encoded. Please enter a name for this idea: "
-idea_name = gets.strip
-File::open( "./ideas/idea-" + idea_name + ".txt", "w") do |f|
-  f << idea.upcase.reverse.gsub!(" ", "")
+def decode_ideas
+  # Print each idea with the words fixed
+  Dir["./ideas/idea-*.txt"].each do |file_name|
+    idea = File::read (file_name)
+    idea.downcase!
+    idea.reverse!
+    
+    CODE_WORDS.each do |real, code|
+      idea.gsub!(code.gsub(" ", "").downcase, real)
+    end
+    
+    puts idea
+  end
 end
+
+# encodes_idea
+decode_ideas
