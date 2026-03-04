db22a2ce5bc51fdc3496406825c47a43dd028f9c
rebecaaras <rebecaaraujo2013@gmail.com>
2026-03-04 15:32:53 -0300

    add wipe_mutterings_from function

---
+++ chapter-5/wipe_mutterings.rb
@@ -0,0 +1,14 @@
+def wipe_mutterings_from( sentence )
+  unless sentence.respond_to? :include? #checks if the object has the include method
+    raise ArgumentError,
+      "cannot wipe mutterings from a #{ sentence.class }"
+  end
+
+  while sentence.include? "("
+    open = sentence.index( "(" )
+    close = sentence.index( ")", open) #open here is the offset
+    sentence[open..close] = "" if close
+  end
+  sentence.gsub("  ", " ")
+end
+
