c327ea6313600a1d375290ae5826a64cc9c58bcd
rebecaaras <rebecaaraujo2013@gmail.com>
2026-03-10 23:12:38 -0300

    feat: generates ticket numbers randomly if not provided

--- chapter-5/lottery_ticket.rb
+++ chapter-5/lottery_ticket.rb
@@ -1,18 +1,21 @@
 class LotteryTicket
   NUMERIC_RANGE = 1..25
 
-  attr_reader :picks, :purchased # what does attr_reader do?? Sets the getters for class var
+  attr_reader :picks, :purchased # what does attr_reader do?? Sets the getters for instance variables
+  private attr_writer :picks
 
   def initialize( *picks ) # * means any arguments will be passed as an array
-    if picks.length != 3
-      raise ArgumentError, "three numbers must be picked"
-    elsif picks.uniq.length != 3
-      raise ArgumentError, "three different numbers must be picked"
-    elsif picks.detect { |p| not NUMERIC_RANGE.include?(p) }
-      raise ArgumentError, "numbers must be in the range of 1 to 25"
-    end
+    picks = self.class.new_random if picks.empty?
+    
+    raise ArgumentError, "three numbers must be picked" unless picks.length === 3
+    raise ArgumentError, "three different numbers must be picked" unless picks.uniq.length === 3
+    raise ArgumentError, "numbers must be in the range of 1 to 25" unless picks.all? { |p| NUMERIC_RANGE.include?(p) }
 
     @picks = picks
     @purchased = Time.now
+  end
+
+  def self.new_random
+    Array.new(3) { rand(1..25) }
   end
 end
